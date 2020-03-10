75b64176f8c0c88072486a10be84035293ffbd70
precious-adeyinka <pflash90@gmail.com>
2020-03-11 00:12:11 +0100

    Updates

---
+++ precache-manifest.0c34fb69c42417ac87ba00cda0702865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867982d00e0628ed53c078c9388d22b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80fdc6760bf2d8758e12",
+    "url": "/static/css/main.beb9224f.chunk.css"
+  },
+  {
+    "revision": "015d75d7529a40ee504d",
+    "url": "/static/js/2.22b8f73d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.22b8f73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80fdc6760bf2d8758e12",
+    "url": "/static/js/main.7dab785e.chunk.js"
+  },
+  {
+    "revision": "bf6b6421e90c2c0e751e",
+    "url": "/static/js/runtime-main.6da2213b.js"
+  }
+]);
